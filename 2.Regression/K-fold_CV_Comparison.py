66f1693fc65796f67a2e7efd190c23ed0b7c61d3
Anika-Tahsin-S <anika.tahsin5@g.bracu.ac.bd>
2022-01-21 00:27:42 +0600

    Initial commit

---
+++ 2.Regression/K-fold_CV_Comparison.py
@@ -0,0 +1,18 @@
+# Import necessary modules
+from sklearn.model_selection import cross_val_score
+from sklearn.linear_model import LinearRegression
+
+# Create a linear regression object: reg
+reg = LinearRegression()
+
+# Perform 3-fold CV
+cvscores_3 = cross_val_score(reg, X, y, cv = 3)
+print(np.mean(cvscores_3))
+
+# Perform 10-fold CV
+cvscores_10 = cross_val_score(reg, X, y, cv = 10)
+print(np.mean(cvscores_10))
+
+# %timeit used to see how long each 3-fold CV takes compared to 10-fold CV
+%timeit cross_val_score(reg, X, y, cv = 3)
+%timeit cross_val_score(reg, X, y, cv = 10)
