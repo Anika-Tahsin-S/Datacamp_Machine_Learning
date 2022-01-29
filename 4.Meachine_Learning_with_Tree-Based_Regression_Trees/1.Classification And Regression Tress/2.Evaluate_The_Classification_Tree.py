76c6ed603f810f12b351400588278bd82a5adfc9
Anika Tahsin <71835381+Anika-Tahsin-S@users.noreply.github.com>
2022-01-29 17:17:15 +0600

    Delete 4.Meachine_Learning_with_Tree-Based_Regression_Trees directory

--- 4.Meachine_Learning_with_Tree-Based_Regression_Trees/1.Classification And Regression Tress/2.Evaluate_The_Classification_Tree.py
+++
@@ -1,9 +0,0 @@
-# Import accuracy_score
-from sklearn.metrics import accuracy_score
-
-# Predict test set labels
-y_pred = dt.predict(X_test)
-
-# Compute test set accuracy  
-acc = accuracy_score(y_test, y_pred)
-print("Test set accuracy: {:.2f}".format(acc))
