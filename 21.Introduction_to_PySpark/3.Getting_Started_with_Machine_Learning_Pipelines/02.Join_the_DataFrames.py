4098a623e71ba098a74b2530d48f6a33471b9d7a
Anika-Tahsin-S <anika.tahsin5@g.bracu.ac.bd>
2022-04-02 00:24:40 +0600

    Initial Commit

--- 21.Introduction_to_PySpark/3.Getting_Started_with_Machine_Learning_Pipelines/02.Join_the_DataFrames.py
+++ 21.Introduction_to_PySpark/3.Getting_Started_with_Machine_Learning_Pipelines/02.Join_the_DataFrames.py
@@ -4,4 +4,5 @@ planes = planes.withColumnRenamed('year', 'plane_year')
 # Join the DataFrames
 model_data = flights.join(planes, on = "tailnum", how = "leftouter")
 
+
 # ==================================================================== #
